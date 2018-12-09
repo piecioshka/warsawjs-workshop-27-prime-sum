645454113c1cf1018a1d3c2982ccf8afb315ce4e
Piotr Kowalski <piecioshka@gmail.com>
2018-12-09 13:58:11 +0100

    Create function: primeSum

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-module.exports = {
-    run() {
-        console.log('Hello world');
+const API = {
+    primeSum(array = []) {
+        return API.sum(array.filter(API.isPrime));
+    },
+    sum(arr) {
+        return arr.reduce((a, b) => a + b, 0);
+    },
+    isPrime(num) {
+        if (num === 0 || num === 1) {
+            return false;
+        }
+        for (let i = 2; i <= num / 2; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
+
+module.exports = API;
--- test/test.index.js
+++ test/test.index.js
@@ -1,9 +1,40 @@
 'use strict';
 
 const { expect } = require('chai');
+const { primeSum, sum, isPrime } = require('../src/index');
 
 describe('General', () => {
-    it('should works', () => {
-        expect(1).to.be.equal(1);
+    it('should work', () => {
+        expect(primeSum([])).to.be.equal(0);
+    });
+    it('primeSum returns number', () => {
+        expect(typeof primeSum()).to.be.equal('number');
+    });
+    it('primeSum returns number', () => {
+        expect(primeSum([2, 3, 4, 5])).to.be.equal(10);
+    });
+});
+
+describe('sum number in array', () => {
+    it('sum []', () => {
+        expect(sum([])).to.be.equal(0);
+    });
+    it('sum 1,2', () => {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+});
+
+describe('check is prime ', () => {
+    it('isPrime 2', () => {
+        expect(isPrime(2)).to.be.equal(true);
+    });
+    it('isPrime 4', () => {
+        expect(isPrime(6)).to.be.equal(false);
+    });
+    it('isPrime 4', () => {
+        expect(isPrime(0)).to.be.equal(false);
+    });
+    it('isPrime 4', () => {
+        expect(isPrime(1)).to.be.equal(false);
     });
 });
